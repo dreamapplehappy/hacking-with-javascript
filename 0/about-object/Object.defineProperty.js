6efcf289c7007431a2979c98877080b03a969b83
dreamapple <2451731631@qq.com>
2016-08-04 10:17:43 +0800

    update ng2 markdown

---
+++ 0/about-object/Object.defineProperty.js
@@ -0,0 +1,26 @@
+var age = 33;
+var obj = {};
+obj.name = 'dreamapple';
+Object.defineProperty(obj, 'age', {
+    //value: null,
+    enumerable: true,
+    configurable: false,
+    //writable: false,
+    get: function() {
+        return age;
+    },
+    set: function(value) {
+        console.log('set', value);
+        age = value + 1;
+    }
+});
+
+console.log(obj);
+
+delete obj.name;
+obj.age = 30;
+
+delete obj.age;
+
+console.log(obj, obj.age);
+
--- series-tutorial/ng2/README.md
+++ series-tutorial/ng2/README.md
@@ -1,1 +1,2 @@
-## 敬请期待
++ 博客: [hacking-with-angular](https://hacking-with-angular.github.io/)
++ 项目: [angular2-travel](https://github.com/hacking-with-angular/angular2-travel)
