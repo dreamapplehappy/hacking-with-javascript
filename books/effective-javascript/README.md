308f030bf36a3e608315d4848c8378655268d597
dreamapple <2451731631@qq.com>
2016-11-01 21:37:40 +0800

    add effective-javascript 1

--- books/effective-javascript/README.md
+++ books/effective-javascript/README.md
@@ -1,6 +1,7 @@
 # Effective JavaScript
 + [ ] Chapter 1
-    - [ ] [Item 1: Know Which JavaScript You Are Using](chapter-1/know-which-javascript-you-are-using.md) 
+    - [x] [Item 1: Know Which JavaScript You Are Using](chapter-1/know-which-javascript-you-are-using.md) 
+    - [x] [Item 2: Understand JavaScript’s Floating-Point Numbers](chapter-1/understand-javascript’s-floating-point-numbers.md) 
 + [ ] Chapter 2
     - [ ] []()    
 + [ ] Chapter 3
---
+++ books/effective-javascript/chapter-1/item2/demo.js
@@ -0,0 +1,22 @@
+console.log(typeof 36); // number
+console.log(typeof 36.36); // number
+console.log(typeof -36.36); // number
+
+console.log(0.1 * 3.6); // 0.36000000000000004
+console.log(1 * 36); // 36
+console.log(1 - 0.6); // 0.4
+console.log(25 / 5); // 5
+console.log(2.5 / 5); // 0.5
+console.log(25 % 4); // 1
+console.log(25 % 0.4); // 0.19999999999999862
+console.log(8 | 1); // 9
+
+console.log((10).toString(2)); // 1010
+console.log(parseInt('1010', 2)); // 10
+
+console.log(0.1 + 0.2); // 0.30000000000000004
+console.log(0.1 + (0.2 + 0.3)); // 0.6
+console.log((0.1 + 0.2) + 0.3); // 0.6000000000000001
+
+console.log(10 + (20 + 30)); // 60
+console.log((10 + 20) + 30); // 60
